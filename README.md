4ad6b11cdc583766ae469d1d8a30281fd543cba9
Matthew Rosca-Halmagean <matthew@rosca-halmagean.com>
2018-02-26 10:54:34 -0600

    Completed project
    Upload of completed project from 2016.

--- README.md
+++ README.md
@@ -1,1 +1,131 @@
 # Key-Sprint
+
+_  __            ____             _       _   
+| |/ /___ _   _  / ___| _ __  _ __(_)_ __ | |_
+| ' // _ \ | | | \___ \| '_ \| '__| | '_ \| __|
+| . \  __/ |_| |  ___) | |_) | |  | | | | | |_
+|_|\_\___|\__, | |____/| .__/|_|  |_|_| |_|\__|
+         |___/        |_|                     
+
+Hey! Thanks for your interest in Key Sprint!
+
+
+In this file, we'll be discussing a few things:
+
+* Compatibility
+* Requirements
+* Installation
+* Tutorial
+* Troubleshooting
+* FAQ
+* Credits
+
+
+
+COMPATIBILITY
+-------------
+
+This script has been written and tested for:
+
+* OS X Yosemite Version 10.10.3 (https://support.apple.com/kb/DL1804)
+
+We do not provide support for any platforms not listed above.
+
+
+
+REQUIREMENTS
+------------
+
+This script requires the following:
+
+* Python 2.7.10 (https://www.python.org/downloads/release/python-2710/)
+* Python sys module (https://docs.python.org/2/library/sys.html)
+* Python time module (https://docs.python.org/2/library/time.html)
+* Python os module (https://docs.python.org/2/library/os.html)
+* Python string module (https://docs.python.org/2/library/string.html)
+* Python random module (https://docs.python.org/2/library/random.html)
+* Python getch module (https://pypi.python.org/pypi/getch)
+* Python getpass module (https://docs.python.org/2/library/getpass.html)
+* Python subprocess module (https://docs.python.org/2/library/subprocess.html)
+
+
+
+INSTALLATION
+------------
+
+All Apple computers ship with Python 2.7.10 installed.
+Most of the required modules for Key Sprint to function properly should also come pre-installed.
+
+If for some reason your computer does not have Python 2.7.10, then you'll need to do the following:
+
+1. Visit https://www.python.org/downloads/release/python-2710/
+2. Select "Mac OS X 64-bit/32-bit installer"
+3. Once the download is completed, just run the file and you'll be fitted with a clean install of Python 2.7.10
+
+You will need to manually install the Python getch module included in your download.
+To do this, please complete the following:
+
+1. Navigate to your KeySprint folder through Terminal
+2. Type "cd requirements/getch" into Terminal
+3. Type "python setup.py install"
+
+
+
+
+TUTORIAL
+--------
+
+To start your Computer Tools script, you'll need to do the following:
+
+1. Move the folder "KeySprint" to your Desktop.
+2. Open the "Terminal" application
+3. Type "cd Desktop" into Terminal and press Enter
+4. Type "cd KeySprint" into Terminal and press Enter
+5. Type "python game.py" into Terminal and press Enter
+6. Done! The script should start and provide further instructions.
+
+
+
+TROUBLESHOOTING
+---------------
+
+* If Terminal's output included "No such file or directory" at any point:
+
+ - Are you sure you moved the "KeySprint" folder to your Desktop?
+
+ - Did you rename the "KeySprint" folder? If so, please change the name back to "KeySprint".
+
+* If the game didn't run when you typed "python game.py":
+
+ - Are you sure you have Python 2.7.10 installed?
+
+
+
+FAQ
+---
+
+* How do I reset the highscore?
+
+ - Simply delete the "highscore.txt" file from the "KeySprint" folder
+
+
+
+CREDITS
+-------
+
+* Contributers
+
+ - Matthew Rosca-Halmagean
+ - Rachel Connelly
+
+* Instructors
+
+ - Mr. Wegscheid
+
+* Big text formatter
+
+ - www.bigtext.org
+
+* Python-related questions
+
+ - www.stackoverflow.com
--- game.py
+++ game.py
@@ -1,64 +1,339 @@
-import time
+################################################################################################
+#                               _   _  ___ _____ _____ ____                                    #
+#                              | \ | |/ _ \_   _| ____/ ___|                                   #
+#                              |  \| | | | || | |  _| \___ \                                   #
+#                              | |\  | |_| || | | |___ ___) |                                  #
+#                              |_| \_|\___/ |_| |_____|____/                                   #
+#                                                                                              #
+#                                                                                              #
+# Key Sprint: How fast are your fingers?                                                       #
+# Created by Matthew Rosca-Halmagean & Rachel Connelly                                         #
+#                                                                                              #
+# All of code written in this program is licensed under the MIT License.                       #
+#                                                                                              #
+# Code has been been written and tested for OS X Yosemite Version 10.10.3 using Python 2.7.10. #
+# We do not provide support for any platforms other than the ones listed above.                #
+#                                                                                              #
+# Please read the README.txt file included in your download for more information.              #
+#                                                                                              #
+################################################################################################
+
+
+# Import modules that will be referenced in the code
 from sys import argv
-script, HighScore.txt = argv
+import time
+import os
+import string
+import random
+import getch
+import getpass
+import subprocess
 
-# start menu
-print("Welcome ...")
-userName = raw_input("First, please enter your name.")
 
-def startGame():
-    print("In this game, the program is going to give you a key to press on your keyboard.")
+# Version number
+__version__ = '0.1.0'
+
+
+# Define HighScore file as HighScore.txt
+HighScore = 'HighScore.txt'
+
+# Global variables
+score = 0
+correct = 0
+incorrect = 0
+
+# Get name of user
+account_user = getpass.getuser()
+
+# Define path to KeySprint directory
+source = os.getcwd()
+
+# Check files in source directory
+for files in source:
+    # Check to find HighScore.txt file
+    if not os.path.exists(source + '/HighScore.txt'):
+        # Create HighScore.txt file
+        create = open(HighScore, 'a')
+
+        # Write current high score as 0
+        create.write('0')
+
+        # Skip one line in the file
+        create.write('\n')
+
+        # Set high score user as null
+        create.write('null')
+
+        # Close HighScore.txt file
+        create.close()
+
+# Define audio paths
+correct_audio = source + '/Sounds/correct.wav'
+incorrect_audio = source + '/Sounds/incorrect.wav'
+intro_audio = source + '/Sounds/intro.wav'
+select_audio = source + '/Sounds/select.wav'
+
+
+# Prompt for user's name
+userName = raw_input("\nFirst, please enter your name.\n")
+print("\nWelcome, " + userName + "!")
+
+# Play intro sound
+subprocess.call(["afplay", intro_audio])
+
+
+# Start menu that begins the game
+def start():
+    # Directions and "How to Play"
+    print("\nIn this game, the program is going to give you a key to press on your keyboard.")
     print("Once you've done that, you're going to be given another key to press.")
-    print("Your goal is to get as many keys pressed correctly as you can in 60 seconds.")
-    beginPlay = input("Would you like to play the game?")
-    beginPlay = beginPlay.lower()
-    if beginPlay == "yes":
-        print("Good luck!")
-        time.sleep(.5)
-        nextScreen = gamePlay
-    elif beginPlay == "no":
-        print("See you next time!")
-        nextScreen = quit()
-    else:
-        print("Please reply with \"yes\" or \"no\".")
-        beginPlay()
+    print("Your goal is to get as many keys pressed correctly as you can in 10 seconds.")
+
+    # Wait two seconds
+    time.sleep(2)
+
+    # Listens for key press
+    # Only proceed once the user presses a key
+    os.system('read -s -n 1 -p "Press any key to continue.\n"')
+
+    # Play selection sound
+    subprocess.call(["afplay", select_audio])
+
+    # Bring user to countdown function
+    countdown()
 
 
-# play again
-def playAgain():
-    gameOver = input("Would you like to play again?")
-    gameOver = gameOver.lower()
-    if gameOver == "yes":
-        print("Good luck!")
-        time.sleep(.5)
-        nextScreen = startGame
-    elif gameOver == "no":
-        print("See you next time!")
-    else:
-        print("Please reply with \"yes\" or \"no\".")
-        gameOver()
+# Countdown timer
+# Starts game immediately afterwards
+def countdown():
+    # Calls global variables
+    global score, correct, incorrect
+
+    # Resets global variables
+    score = 0
+    correct = 0
+    incorrect = 0
+
+    # Countdown from five
+    # Wait 0.5 seconds between number
+    print("\n5")
+    time.sleep(0.5)
+    print("4")
+    time.sleep(0.5)
+    print("3")
+    time.sleep(0.5)
+    print("2")
+    time.sleep(0.5)
+    print("1")
+
+    # Finish countdown
+    print("GO!\n")
+
+    # Bring user to core function
+    core()
 
 
-# high score
+# Contains all game functionality
+def core():
+    # Calls global variables
+    global score, correct, incorrect
 
-def scoreBoard():
-    target = open(HighScore.txt)
-    target.truncate()
-    currentHighScore = target.read(line1)
-    currentHighScore = int(currentHighScore)
-    if userScore > currentHighScore:
-        target.write(line1)
-        print("Congratulations! You got the new high score!")
-        time.sleep(.5)
-        playAgain()
-    elif userScore == currentHighScore:
-        print("You met the high score, but you did not exceed it.")
-        time.sleep(.5)
-        playAgain()
-    else:
-        print("Sorry, you did not get the high score.")
-        print("The current high score is: ", currentHighScore)
-        time.sleep(.5)
-        playAgain()
+    # Define start time
+    begin = time.time()
+
+    # Loops until 10 seconds are up
+    # Subtracts start time from current time
+    # Continue loop while less than 10
+    while time.time() - begin < 10:
+
+        # Select a random character
+        task = random.choice(string.ascii_uppercase)
+
+        # Print random character
+        print task
+
+        # Get character that user presses
+        k1 = getch.getch()
+
+        # Convert letter to uppercase
+        k2 = k1.upper()
+
+        # Compare user character to random character
+        # Characters match
+        if k2 == task:
+            # Add six points to user score
+            score += 6
+
+            # Add one to correct total
+            correct += 1
+
+        # Compare user character to random character
+        # Characters do not match
+        if k2 != task:
+            # Subtract three points from user score
+            score -= 3
+
+            # Add one to incorrect total
+            incorrect += 1
+
+    # Bring users to scores function
+    scores()
+
+
+# Score checking and high scores
+def scores():
+    # Calls global variables
+    global score, correct, incorrect
+
+    # Convert correct and incorrect totals
+    # From integer to string
+    correct = str(correct)
+    incorrect = str(incorrect)
+
+    # Open HighScore.txt file
+    target = open(HighScore)
+
+    # Read file by line individually
+    lines = target.readlines()
+
+    # Pull high score from line one
+    # Define as high score variable
+    high = lines[0]
+
+    # Convert high score to integer
+    high = int(high)
+
+    # Pull high score user from line two
+    # Define as high score user variable
+    holder = lines[1]
+
+    # Close HighScore.txt file
     target.close()
 
+    # Check if user score beats high score
+    if score > high:
+        # Convert user score to string
+        score = str(score)
+
+        # Open HighScore.txt file
+        target = open(HighScore, 'w')
+
+        # Wipe HighScore.txt file
+        target.truncate()
+
+        # Add user score to line one
+        # User score is the new high score
+        target.write(score)
+
+        # Skip one line in the file
+        target.write("\n")
+
+        # Add user name to line two
+        # User name is the new high score name
+        target.write(userName)
+
+        # Close HighScore.txt file
+        target.close()
+
+        # Inform user they beat the high score
+        # Inform user the score they received
+        # Inform user how many correct and incorrect
+        print("\nCongratulations, " + userName + "! You got the new high score!\n\nYour score was " + score + ".\n")
+        print("You got " + correct + " correct and " + incorrect + " incorrect.\n")
+
+        # Play correct audio
+        subprocess.call(["afplay", correct_audio])
+
+        # Wait 0.5 seconds
+        time.sleep(.5)
+
+        # Bring user to retry function
+        retry()
+
+    # Check if user score is equal to high score
+    if score == high:
+        # Convert user score to string
+        score = str(score)
+
+        # Inform user they tied the high score
+        # Inform user the score they received
+        # Inform user how many correct and incorrect
+        print("\nYou met the high score, but you did not exceed it.\n\nYour score was " + score + ".\n")
+        print("You got " + correct + " correct and " + incorrect + " incorrect.\n")
+
+        # Play incorrect audio
+        subprocess.call(["afplay", incorrect_audio])
+
+        # Wait 0.5 seconds
+        time.sleep(.5)
+
+        # Bring user to retry function
+        retry()
+
+    # Check if user score is less than high score
+    if score < high:
+        # Convert high score to string
+        high = str(high)
+
+        # Convert user score to string
+        score = str(score)
+
+        # Inform user they did not beat the high score
+        # Inform user the score they received
+        # Inform user how many correct and incorrect
+        # Inform user the high score and high score user
+        print("\nSorry, you did not get the high score.\n\nYour score was " + score + ".\n")
+        print("You got " + correct + " correct and " + incorrect + " incorrect.\n")
+        print("The current high score is " + high + " and is held by " + holder + ". Better luck next time!\n")
+
+        # Play incorrect audio
+        subprocess.call(["afplay", incorrect_audio])
+
+        # Wait 0.5 seconds
+        time.sleep(.5)
+
+        # Bring user to retry function
+        retry()
+
+    # Bring user to retry function
+    retry()
+
+
+# Prompts user to restart or quit
+def retry():
+    # Ask user to play again
+    over = raw_input("\nWould you like to play again?\n")
+
+    # Convert user input to lowercase
+    over = over.lower()
+
+    # If user input is yes
+    if over == "yes":
+        # Print restart message
+        print("\nRestarting Key Sprint...")
+
+        # Wait 0.5 seconds
+        time.sleep(.5)
+
+        # Bring user to countdown function
+        countdown()
+
+    # If user input is no
+    if over == "no":
+        # Print quit message
+        print("\nSee you next time!\n")
+
+        # Quit game.py
+        quit()
+
+    # If user input is unknown
+    else:
+        # Print help message
+        print("\nPlease reply with \"yes\" or \"no\".\n")
+
+        # Restart retry function
+        retry()
+
+# Call start function
+# Begin game
+start()
